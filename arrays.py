30480d5b323ac72b76fcfc6a6aa66d6c518fac3a
changdukkim <33943750+changdukkim@users.noreply.github.com>
2018-08-12 00:53:56 +0900

    Add files via upload

---
+++ arrays.py
@@ -0,0 +1,26 @@
+numbers = [10, 20, 300, 40.5, 50];
+
+# random indexing --> O(1) get items if we know the index !!!
+# print(numbers[4]);
+# numbers[1] = 'Adam';
+# print(numbers[1]);
+
+#for num in numbers:
+#    print(num);
+#for i in range(len(numbers)):
+#    print(numbers[i]);
+
+# print(numbers[:-2]);
+
+# O(N) search running time
+maximum = numbers[0];
+
+for num in numbers:
+    if num > maximum:
+        maximum = num;
+
+#for i in range(5):
+#    if numbers[i] > maximum:
+#        maximum = numbers[i];
+
+print(maximum);
